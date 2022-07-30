23945a2dbfabf38a01ef6b63964a560acf04d2f9
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-07-30 15:31:42 +0100

    two greens returns green:2

--- Exercise15-school-reports/schoolReports.js
+++ Exercise15-school-reports/schoolReports.js
@@ -1,6 +1,8 @@
 const schoolReports = (colour) => { 
     if(colour == "Green") {
         return "Green:1"
+    }else if(colour == "Green, Green"){
+        return "Green:2"
     }
 
 
--- Exercise15-school-reports/schoolReports.test.js
+++ Exercise15-school-reports/schoolReports.test.js
@@ -3,4 +3,7 @@ describe('green', () => {
     it('checks that green returrns Green: 1', () => {
         expect(schoolReports("Green")).toEqual("Green:1")
     })
+    it('checks that two greens return Green:2', () => {
+        expect(schoolReports("Green, Green")).toEqual("Green:2")
+    })
 })
