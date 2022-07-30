455348e48df9d516dc8490b62d02b8e903829ec6
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-07-30 14:37:19 +0100

    returns buzz if number is 5

--- Exercise1-fizbuzz/fizzbuzz.js
+++ Exercise1-fizbuzz/fizzbuzz.js
@@ -1,11 +1,9 @@
 const fizzbuzz = (number) => {
-    if(number == 1) {
-        return "1"
-    } else if(number == 3) {
+     if(number % 3 == 0) {
         return "fizz"
-    } else if(number == 5) {
+    } else if(number % 5 == 0) {
         return "buzz"
-    }
+    } else return number.toString()
 }
 
 module.exports = fizzbuzz
