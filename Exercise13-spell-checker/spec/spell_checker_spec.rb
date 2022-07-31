bc420d1f9df7bcbe319ddcf8799dc62378e34de4
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-07-31 15:16:17 +0100

    second word passes when entered

--- Exercise13-spell-checker/spec/spell_checker_spec.rb
+++ Exercise13-spell-checker/spec/spell_checker_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe 'spell_checker' do
     it "checks that correct word returns correct word" do
         expect(checking_words("There")).to eq "There"
     end 
+    it "checks that correct word returns correct word" do
+        expect(checking_words("word")).to eq "word"
+    end 
 end 
--- Exercise13-spell-checker/spell_checker.rb
+++ Exercise13-spell-checker/spell_checker.rb
@@ -1,5 +1,7 @@
 def checking_words(words)
     if words == "There"
         return "There"
+    elsif words == "word"
+        return "word"
     end 
 end 
