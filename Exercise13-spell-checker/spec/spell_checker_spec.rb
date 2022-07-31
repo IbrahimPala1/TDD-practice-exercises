dcc2f3a9683af3b1585043c9907622ca7fd169ee
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-07-31 15:33:48 +0100

    refactored wroing words

--- Exercise13-spell-checker/spec/spell_checker_spec.rb
+++ Exercise13-spell-checker/spec/spell_checker_spec.rb
@@ -11,12 +11,15 @@ RSpec.describe 'spell_checker' do
         expect(checking_words("was")).to eq "was"
     end 
     it "checks that correct word returns correct word" do
-        expect(checking_words("smenlt")).to eq "~smnelt~"
+        expect(checking_words("smenlt")).to eq "~smenlt~"
     end 
     it "checks that correct word returns correct word" do
         expect(checking_words("blet")).to eq "~blet~"
     end 
     it "checks that correct word returns correct word" do
         expect(checking_words("yse")).to eq "~yse~"
+    end 
+    it "checks that correct word returns correct word" do
+        expect(checking_words("There was")).to eq "There was"
     end 
 end 
--- Exercise13-spell-checker/spell_checker.rb
+++ Exercise13-spell-checker/spell_checker.rb
@@ -3,11 +3,7 @@ def checking_words(words)
     word_bank = ["There", "word", "was"]
     if word_bank.include?(words)
         return words
-    elsif words == 'smenlt'
-        return "~smnelt~"
-    elsif words == 'blet'
-        return "~blet~"
-    elsif words == 'yse'
-        return "~yse~"
+    else
+        return "~" +words + "~"
     end 
 end 
