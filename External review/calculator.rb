88998f1c74bc89fb0fed9af19baebf4daf15b7a2
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-08-08 13:23:36 +0100

    second number gets the desired output

--- External review/calculator.rb
+++ External review/calculator.rb
@@ -1,5 +1,7 @@
 def calculator(string)
   if string == "1"
-    return "1"
+    return ['1', 1]
+  elsif string == '4'
+    return ['4', 4]
   end 
 end 
--- External review/spec/calculator_spec.rb
+++ External review/spec/calculator_spec.rb
@@ -1,9 +1,10 @@
 require './calculator'
 RSpec.describe 'calculator' do 
     it 'checks that one number string returns a numbered string' do
-        expect(calculator("1")).to eq "1"
+        expect(calculator("1")).to eq ["1", 1]
     end 
     it 'checks that one number string returns a numbered string' do
-        expect(calculator("4")).to eq "4"
+        expect(calculator("4")).to eq ["4", 4]
     end 
+    
 end 
