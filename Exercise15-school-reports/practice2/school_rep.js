814976651fb812e454cd64943690642e73e49e9e
Esra Kaya <ibrahimpala2019@gmail.com>
2022-09-08 20:11:39 +0100

    amber working

--- Exercise15-school-reports/practice2/school_rep.js
+++ Exercise15-school-reports/practice2/school_rep.js
@@ -1,5 +1,11 @@
 const school_reports = (grades) => {
-    return grades + ': 1'
+    if(grades == 'Green') {
+        return 'Green: 1'
+    } else if(grades == 'Amber') {
+        return 'Amber: 1'
+    } else {
+        return false 
+    }
 }
 
 module.exports = school_reports
--- Exercise15-school-reports/practice2/school_rep.test.js
+++ Exercise15-school-reports/practice2/school_rep.test.js
@@ -3,5 +3,8 @@ const school_reports = require('./school_rep')
 describe('school_reports', () => {
     it('checks if green returna green: 1', () => {
         expect(school_reports('Green')).toBe('Green: 1')
-})
+     })
+     it('checks if green returna green: 1', () => {
+        expect(school_reports('Amber')).toBe('Amber: 1')
+     })
 })
