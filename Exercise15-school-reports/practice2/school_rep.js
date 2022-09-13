303a2110eaffcbf52f064a42b16b4c8b4798608c
Esra Kaya <ibrahimpala2019@gmail.com>
2022-09-13 15:08:50 +0100

    finished school reports challenge

--- Exercise15-school-reports/practice2/school_rep.js
+++ Exercise15-school-reports/practice2/school_rep.js
@@ -1,15 +1,23 @@
 const school_reports = (grades) => {
-    splitting = grades.split(',')
-    len = splitting.length
-    if(grades == 'Green, Amber') {
-        return 'Green: 1, Amber: 1'
-    }else if(grades == 'Red, Amber') {
-        return 'Red: 1, Amber: 1'
-    }else if(grades == 'Red, Green') {
-        return 'Red: 1, Green: 1'
-    }else {
-    return splitting[0] + ': ' + len
-    }
+    splitting = grades.split(', ')
+    green = 0 
+    amber = 0
+    red = 0
+    for(element of splitting) {
+        upper = element[0].toUpperCase() + element.toLowerCase().slice(1)
+        console.log(upper)
+      if(upper == 'Green') {
+          green += 1
+      }else if(upper === 'Red') {
+          red += 1
+      }else if(upper == 'Amber') {
+          amber += 1
+      } else {
+        return "Incorrect input"
+      }
+    } return "Green: " + green + ', ' + "Amber: " + amber + ', ' + "Red: " + red
 }
 
-module.exports = school_reports
+
+
+module.exports = school_reports 
--- Exercise15-school-reports/practice2/school_rep.test.js
+++ Exercise15-school-reports/practice2/school_rep.test.js
@@ -2,33 +2,49 @@ const school_reports = require('./school_rep')
 
 describe('school_reports', () => {
     it('checks if green returna green: 1', () => {
-        expect(school_reports('Green')).toBe('Green: 1')
+        expect(school_reports('Green')).toBe('Green: 1, Amber: 0, Red: 0')
      })
      it('checks if amber returna amber: 1', () => {
-        expect(school_reports('Amber')).toBe('Amber: 1')
+        expect(school_reports('Amber')).toBe('Green: 0, Amber: 1, Red: 0')
      })
      it('checks if red returns red:1', () => {
-        expect(school_reports('Red')).toBe('Red: 1')
+        expect(school_reports('Red')).toBe('Green: 0, Amber: 0, Red: 1')
      })
      it('checks if red returns red:1', () => {
-        expect(school_reports('Red')).toBe('Red: 1')
+        expect(school_reports('Red')).toBe('Green: 0, Amber: 0, Red: 1')
      })
      it('checks if two red returns red:2', () => {
-        expect(school_reports('Red, Red')).toBe('Red: 2')
+        expect(school_reports('Red, Red')).toBe('Green: 0, Amber: 0, Red: 2')
      })
      it('checks if two amber returns amber:2', () => {
-        expect(school_reports('Amber, Amber')).toBe('Amber: 2')
+        expect(school_reports('Amber, Amber')).toBe('Green: 0, Amber: 2, Red: 0')
      })
      it('checks if two amber returns amber:2', () => {
-        expect(school_reports('Green, Green')).toBe('Green: 2')
+        expect(school_reports('Green, Green')).toBe('Green: 2, Amber: 0, Red: 0')
      })
      it('checks if two different colors returns amber:1, green:1', () => {
-        expect(school_reports('Green, Amber')).toBe('Green: 1, Amber: 1')
+        expect(school_reports('Green, Amber')).toBe('Green: 1, Amber: 1, Red: 0')
      })
      it('checks if two different colors returns red:1, amber:1', () => {
-        expect(school_reports('Red, Amber')).toBe('Red: 1, Amber: 1')
+        expect(school_reports('Red, Amber')).toBe('Green: 0, Amber: 1, Red: 1')
      })
      it('checks if two different colors returns Red:1, green:1', () => {
-        expect(school_reports('Red, Green')).toBe('Red: 1, Green: 1')
+        expect(school_reports('Red, Green')).toBe('Green: 1, Amber: 0, Red: 1')
      })
+     it('checks if two different colors returns Red:1, green:1', () => {
+        expect(school_reports('red, Green, Red, Green, Amber')).toBe('Green: 2, Amber: 1, Red: 2')
+     })
+     it('checks if two different colors returns Red:1, green:1', () => {
+        expect(school_reports('red, green, Red, Green, Amber')).toBe('Green: 2, Amber: 1, Red: 2')
+     })
+     it('checks if two different colors returns Red:1, green:1', () => {
+        expect(school_reports('red, greEn, ReD, GreEn, AmbEr')).toBe('Green: 2, Amber: 1, Red: 2')
+     })
+     it('checks if two different colors returns Red:1, green:1', () => {
+        expect(school_reports('Green, black')).toBe('Incorrect input')
+     })
+     it('checks if two different colors returns Red:1, green:1', () => {
+        expect(school_reports('Green, green, red, ReD, amBER, red')).toBe('Green: 2, Amber: 1, Red: 3')
+     })
+     
 })
